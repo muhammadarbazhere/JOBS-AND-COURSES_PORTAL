6ac5700f3bafe90ae4c0e45ecd252d296b5faa00
Muhammad Arbaz <muhammad.arbazhere@gmail.com>
2025-09-09 15:28:27 +0500

    x

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 require("dotenv").config();
 const path = require("path");
